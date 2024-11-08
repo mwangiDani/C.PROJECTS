e0a05fe8f83dcb6088d1a6f4e5f24a4863066410
mwangiDani <gitaridaniel36@gail.com>
2024-11-08 18:17:34 +0300

    update

---
+++ function to check even or odd number.c
@@ -0,0 +1,20 @@
+
+/*
+Name:Mwangi Daniel Gitari
+Reg No:CT101/G/21346/24
+Date:8/11/2024
+*/
+#include<stdio.h>
+int main (){
+	int num;
+	
+	printf("enter a number:");
+	scanf("%d",&num);
+	
+	if (num  %  2 ==0 ){
+	printf("The number is even\n");
+	}	else  {
+		printf("The number is odd\n");}
+
+	return 0;
+}
---
+++ text.files.c
@@ -0,0 +1,50 @@
+// program  using c .files
+/*
+Name:Mwangi Daniel Gitari
+Reg No:CT101/G/21346/24
+Date:8/11/2024
+program  using c files
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	char sentence;
+	FILE*fptr;
+	
+	fptr = fopen( "C:\\Users\\USER\\Desktop\\New folder\\output.txt","w");
+	if (fptr == NULL){
+		printf("error opening a file ");
+		exit(1);
+	}
+	
+
+		printf("enter a sentence of 200 character:");
+		scanf("%c",&sentence);
+		
+		printf(fptr,"%c",sentence);
+		fclose(fptr);
+		
+			fptr = fopen( "C:\\Users\\USER\\Desktop\\New folder\\data.txt","r");
+	if (fptr == NULL){
+		printf("error opening file for reading file ");
+		exit(1);
+	}
+	fclose(fptr);
+	
+	fptr = fopen( "C:\\Users\\USER\\Desktop\\New folder\\data.txt","a");
+	if (fptr == NULL){
+		printf("error opening file for appending file ");
+		exit(1);
+	}
+	
+	
+	
+		
+		
+		
+		
+		return 0;
+}
