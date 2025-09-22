952c74a6615171aa048551ce11d06cf1fd4fd38e
mwangiDani <gitaridaniel005@gmail.com>
2025-09-22 05:37:16 +0300

    first commit

---
+++ NewFile1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, product;
+
+    // Ask the user for two numbers
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Perform multiplication
+    product = num1 * num2;
+
+    // Display result
+    printf("The product of %d and %d is: %d\n", num1, num2, product);
+
+    return 0;
+}
